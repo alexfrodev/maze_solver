1e5121cf28909af3414ebfebd54f0b0cc94129d2
alexfrodev <alex.franco3d@outlook.com>
2025-03-03 19:12:45 -0600

    Add maze class

---
+++ maze.py
@@ -0,0 +1,51 @@
+import time
+from cell import Cell
+
+
+class Maze:
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_cols,
+            cell_size_x,
+            cell_size_y,
+            win,
+    ):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self._cells = []
+        self._create_cells()
+
+    def _create_cells(self):
+        for i in range(self.num_cols):
+            column = []
+            for j in range(self.num_rows):
+                column.append(Cell(self.win))
+            self._cells.append(column)
+
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+                self._draw_cells(i, j)
+
+    def _draw_cells(self, i, j):
+        self._cells[j][i].draw(
+                self.x1 + i * self.cell_size_x,
+                self.y1 + j * self.cell_size_y,
+                self.x1 + self.cell_size_x,
+                self.y1 + self.cell_size_y
+        )
+
+        self._animate()
+
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
+
+
