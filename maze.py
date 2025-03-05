82d216b74ca67fc89fb0d4f60965c22b196374a6
alexfrodev <alex.franco3d@outlook.com>
2025-03-04 18:08:00 -0600

    Add entrance and exit to maze

--- maze.py
+++ maze.py
@@ -22,6 +22,7 @@ class Maze:
         self.win = win
         self._cells = []
         self._create_cells()
+        self._break_entrance_and_exit()
 
     def _create_cells(self):
         for i in range(self.num_cols):
@@ -30,19 +31,18 @@ class Maze:
                 column.append(Cell(self.win))
             self._cells.append(column)
 
-        for i in range(self.num_rows):
-            for j in range(self.num_cols):
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
                 self._draw_cells(i, j)
 
     def _draw_cells(self, i, j):
         if self.win is None:
             return
-        self._cells[j][i].draw(
-                self.x1 + i * self.cell_size_x,
-                self.y1 + j * self.cell_size_y,
-                self.x1 + self.cell_size_x,
-                self.y1 + self.cell_size_y
-        )
+        x1 = self.x1 + i * self.cell_size_x
+        y1 = self.y1 + j * self.cell_size_y
+        x2 = x1 + self.cell_size_x
+        y2 = y1 + self.cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
 
         self._animate()
 
@@ -52,4 +52,9 @@ class Maze:
         self.win.redraw()
         time.sleep(0.05)
 
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cells(0, 0)
+        self._cells[self.num_cols - 1][self.num_rows - 1].has_bottom_wall = False
+        self._draw_cells(self.num_cols - 1, self.num_rows - 1)
 
